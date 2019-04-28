1afd2c40959917545179a2bb8f9af98af38b3ea7
vinnyfs89 <viniciusfesil@gmail.com>
2019-04-28 00:16:37 -0300

    [UPDATE] Setting mongodb connection to moongose.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/leaflet_map', { useNewUrlParser: true }, function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
